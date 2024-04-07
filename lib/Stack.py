8927710a14e8cebf45b0149baa90ca01b1a3387f
RumiYo <yokoyamarumi@gmail.com>
2024-04-07 18:06:58 -0400

    Complete lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,42 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
+        self.items = items
+        self.limit = limit
 
     def isEmpty(self):
-        pass
+        return len(self.items) == 0
 
     def push(self, item):
-        pass
+        if len(self.items) < self.limit:
+            self.items.append(item)
+        else:
+            print("Stach is full. Cannot push more items.")
 
     def pop(self):
-        pass
+        if len(self.items) > 0 :
+            return self.items.pop()
+        else: 
+            return None
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        return len(self.items) >= self.limit
 
     def search(self, target):
-        pass
+        i = len(self.items) - 1
+
+        while i >= 0:
+            if self.items[i] == target: 
+                return len(self.items) - i - 1
+            else:
+                if i > 0:
+                    i -= 1
+                else: 
+                    return -1
+            
